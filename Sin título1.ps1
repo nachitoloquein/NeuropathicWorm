c1364820d6ad694f00aed147d12f2d09795e0368
nachitoloquein <elnachoescachondo@gmail.com>
2022-10-03 01:19:54 -0300

    test

--- Sin título1.ps1
+++
@@ -1,3 +0,0 @@
-﻿
-echo "hola"
-cls
---
+++ shell.ps1
@@ -0,0 +1,28 @@
+﻿[System.Windows.Forms.MessageBox]::Show("Hello Friend", "Hacked!")
+Write-Host "Una cosa"
+echo "Otra Cosa"
+$variable = "Hola" 
+if ($variable -eq "Hola"){
+    echo "Tst"
+}
+else{
+    echo "LOl"
+}
+
+Function Set-WallPaper($Value)
+{
+ Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value $value
+ rundll32.exe user32.dll, UpdatePerUserSystemParameters
+}
+
+Set-WallPaper -value "C:\Users\edosa\Downloads\NeutopathicWorm\fsociety.jpg"
+
+try{
+Set-WallPaper -value "fsociety.jpg"
+}catch{
+  Write-Host "An error occurred:"
+  Write-Host $_
+}
+mkdir troll
+pause
+exit
